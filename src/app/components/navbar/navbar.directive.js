b32035dbb64492457548f4051539846ef8a69858
Nad Flores <floresnadc@gmail.com>
2016-06-27 09:19:44 +0800

    Replace home link with method.

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -3,17 +3,30 @@
 
   angular
     .module('easebooking')
-    .directive('topBar', topBar);
+    .directive('topBar', topBar)
+    .controller('NavController', NavController)
 
   /** @ngInject */
   function topBar() {
     var directive = {
       restrict: 'E',
-      templateUrl: 'app/components/navbar/navbar.html'
+      templateUrl: 'app/components/navbar/navbar.html',
+      controller: 'NavController',
+      controllerAs: 'nav'
     };
 
     return directive;
 
+  }
+
+  function NavController($state){
+    var vm = this;
+
+    vm.home = home;
+
+    function home(){
+      $state.go('home');
+    }
   }
 
 })();
